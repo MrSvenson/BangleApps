c42d647d90b3832b9394da6a290fba8f92a9d63e
MrSvenson <39689292+MrSvenson@users.noreply.github.com>
2022-06-01 12:09:50 +0200

    Update app.js

--- apps/quotes/app.js
+++ apps/quotes/app.js
@@ -1,34 +1,15 @@
-// place your const, vars, functions or classes here
-
-// clear the screen
 g.clear();
-
-var n = 0;
-
-// redraw the screen
-function draw() {
-  g.reset().clearRect(Bangle.appRect);
-  g.setFont("6x8").setFontAlign(0,0).drawString("Up / Down",g.getWidth()/2,g.getHeight()/2 - 20);
-  g.setFont("Vector",60).setFontAlign(0,0).drawString(n,g.getWidth()/2,g.getHeight()/2 + 30);
-}
-
-// Respond to user input
-Bangle.setUI({mode: "updown"}, function(dir) {
-  if (dir<0) {
-    n--;
-    draw();
-  } else if (dir>0) {
-    n++;
-    draw();
-  } else {
-    n = 0;
-    draw();
-  }
-});
-
-// First draw...
-draw();
-
-// Load widgets
-Bangle.loadWidgets();
-Bangle.drawWidgets();
+setWatch(() => {
+  Bluetooth.println(JSON.stringify({t:"http", url:"https://bangle.oskaralmqvist.se/quote.php"}));
+  global.GB = (event) => {
+    if (event.t == "http"){
+      if (event.err)
+        E.showAlert(event.err);
+      else
+        E.showMessage(event.resp+ "");
+      } else {
+        E.showAlert("Event was: " + event.t);
+      }
+    };  
+  setTimeout(()=>Bangle.drawWidgets(), 1000);
+}, BTN1, {repeat:true});
